7404c54883996f19c0157f7456bb720477be4c9f
AldaErni <aldazhar.erniyaz@gmail.com>
2024-05-16 15:18:14 +0500

    pagination added in insight section

--- src/js/components/home/slider.js
+++ src/js/components/home/slider.js
@@ -1,13 +1,18 @@
 import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/pagination';
 
 export const useInsightSlider = () => {
     new Swiper('.insight__slider', {
+        modules: [Navigation, Pagination],
         slidesPerView: 'auto',
         spaceBetween: 32,
         loop: true,
         centeredSlides: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
         breakpoints: {
             993: {
                 centeredSlides: false,
@@ -23,7 +28,6 @@ export const useTestimonialsSlider = () => {
         spaceBetween: 22,
 
         loop: true,
-
         navigation: {
             prevEl: '.testimonials__btn--prev',
             nextEl: '.testimonials__btn--next',
